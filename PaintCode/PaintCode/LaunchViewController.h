a3da3def1fda2b86b665aea738254117aff91540
henvyluk <henvyluk@163.com>
2016-12-23 22:33:24 +0800

    up rm

---
+++ PaintCode/PaintCode/LaunchViewController.h
@@ -0,0 +1,13 @@
+//
+//  LaunchViewController.h
+//  PaintCode
+//
+//  Created by henvy on 23/12/2016.
+//  Copyright © 2016 henvy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LaunchViewController : UIViewController
+
+@end
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # PaintCode
 用PaintCode模仿Twitter的启动界面logo飞出界面的效果。
 
-![](http://p1.bpimg.com/1949/6ac5d4feb3c0c180.gif)
+![](http://p1.bpimg.com/1949/6860f9ce30e5c32e.gif)
